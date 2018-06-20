a8beb42b9e0730806ce04672c111b757800c89c2
cdl08001 <calin.lewis@gmail.com>
2018-06-19 20:24:12 -0700

    Add music

---
+++ src/spinnyDancer.js
@@ -0,0 +1,25 @@
+var SpinnyDancer = function(top, left, timeBetweenSteps) {
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.step(timeBetweenSteps);
+  this.$node = $('<span class="SpinnyDancer w3-spin"></span>');
+  this.setPosition(top, left);
+
+};
+
+
+
+SpinnyDancer.prototype = Object.create(Dancer.prototype);
+SpinnyDancer.constructor = SpinnyDancer;
+
+SpinnyDancer.prototype.step = function(time) {
+
+  Dancer.prototype.step.call(this, time);
+  // this.$node.toggle();
+
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+};
